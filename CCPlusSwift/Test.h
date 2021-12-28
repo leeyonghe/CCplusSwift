8cdd742dbce15592f08a0015624da39c8f97c29c
leeyonghe <lyhdra99@gmail.com>
2021-12-28 18:54:13 +0900

    init

---
+++ CCPlusSwift/Test.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  CCPlusSwift
+//
+//  Created by ios-dev on 2021/12/28.
+//
+
+#ifndef Test_h
+#define Test_h
+
+#include <stdio.h>
+
+void good_swap(int *a, int *b);
+
+#endif /* Test_h */
---
+++ CCPlusSwift/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CCPlusSwift
+//
+//  Created by ios-dev on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.Logic()
+    }
+    
+    func Logic(){
+        
+        let AA = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        AA.pointee = 10;
+        
+        let BB = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        BB.pointee = 20;
+        
+        defer {
+            AA.deallocate()
+            BB.deallocate()
+        }
+        
+        good_swap(AA, BB)
+        
+        print("A = \(AA.pointee), B = \(BB.pointee)")
+        
+    }
+
+
+}
+
